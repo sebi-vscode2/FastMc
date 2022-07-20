c95d8dd949b8d3649602cbb27afa906aa45a8350
sebi-vscode2 <stoklosasebastian114@gmail.com>
2022-07-20 20:55:05 -0300

    Added exit

--- toolsmc.sh
+++ toolsmc.sh
@@ -2,18 +2,26 @@
 # Welcome and help
 
 echo "Welcome To ToolsMc!"
-echo "Write 1  or 2 to navigate on the menu!"
+echo "Write 1, 2 or 3 to navigate on the menu!"
 echo "What do you want to do?"
 echo "1. Go to github" 
 echo "2. Create A Server"
-
+echo "3. Exit"
+read a
+#if for exit
+if  [ $a = 3 ];
+then
+clear
+echo "Exiting!" 
+exit 0
+fi
 
 #if for github
 
-read a
- 
+clear 
 if  [ $a = 1 ];
 then 
+echo "Opening on github"
 xdg-open https://github.com/sebi-vscode2/ToolsMc
 exit 0
 fi
