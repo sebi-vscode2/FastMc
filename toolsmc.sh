cc3e28fccb7a147180ef72bbd8ef60f19e48d718
sebi-vscode2 <stoklosasebastian114@gmail.com>
2022-07-20 20:22:11 -0300

    Added link to github

--- toolsmc.sh
+++ toolsmc.sh
@@ -4,4 +4,10 @@ echo "Write 1  or 2 to navigate on the menu!"
 echo "What do you want to do?"
 echo "1. Go to github" 
 echo "2. Create A Server"
-read a 
+read a
+ 
+if  [ $a = 1 ];
+then 
+xdg-open https://github.com/sebi-vscode2/ToolsMc
+exit 0
+fi 
