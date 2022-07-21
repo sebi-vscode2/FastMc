dd55ceab165ef13bed5fccccc79fad6efce4f9dd
sebi-vscode2 <stoklosasebastian114@gmail.com>
2022-07-21 11:20:27 -0300

    added toolsmc.sh to path

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get install openjdk-17-jdk -y
-mkdir toolsmc
+sudo cd /home
+sudo mkdir toolsmc
 sudo wget -q  https://raw.githubusercontent.com/sebi-vscode2/ToolsMc/master/toolsmc.sh
 sudo chmod +777 ./toolsmc.sh
-mv ./toolsmc.sh toolsmc
+sudo mv ./toolsmc.sh /usr/bin
 clear
 echo "Toolsmc is now installed"
 echo "Now run toolsmc/toolsmc.sh!" 
