d52889e3ba96421d8e1d0bc39340445b528212bc
sebi-vscode2 <stoklosasebastian114@gmail.com>
2022-07-21 14:12:35 -0300

    prepare.sh was made

--- install.sh
+++ install.sh
@@ -6,7 +6,10 @@ sudo mkdir toolsmc
 sudo wget -q  https://raw.githubusercontent.com/sebi-vscode2/ToolsMc/master/toolsmc.sh
 sudo chmod +777 ./toolsmc.sh
 sudo mv ./toolsmc.sh /usr/bin
-sudo wget -q -O- 
+sudo wget -q https://raw.githubusercontent.com/sebi-vscode2/ToolsMc/master/prepare.sh 
+sudo chmod +777 prepare.sh
+sudo ./prepare.sh
+sudo rm prepare.sh
 clear
 echo "Toolsmc is now installed"
 echo "Now run toolsmc/toolsmc.sh!" 
---
+++ prepare.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd /home
+sudo mkdir servers
+cd servers
+sudo mkdir vanilla
+sudo mkdir paper
+sudo mkdir spigot
+sudo mkdir bukkit
+
