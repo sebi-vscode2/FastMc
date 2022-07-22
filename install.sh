42dcdd1187db2922eac4eaad9ea6bc47c649cc90
sebi-vscode2 <stoklosasebastian114@gmail.com>
2022-07-21 23:49:37 -0300

    ad

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get install openjdk-17-jdk -y
-sudo cd /home
+cd /home
 sudo mkdir toolsmc
 sudo wget -q  https://raw.githubusercontent.com/sebi-vscode2/ToolsMc/master/toolsmc.sh
 sudo chmod +777 ./toolsmc.sh
