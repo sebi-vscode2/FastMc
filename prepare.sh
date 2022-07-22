115db72b78faf5c4b9ee51d671c33f67d78dedd3
sebi-vscode2 <stoklosasebastian114@gmail.com>
2022-07-22 10:36:09 -0300

    added .sh files to path

--- prepare.sh
+++ prepare.sh
@@ -9,8 +9,6 @@ sudo mkdir bukkit
 wget  https://raw.githubusercontent.com/sebi-vscode2/ToolsMc/master/bukkit.sh
 wget  https://raw.githubusercontent.com/sebi-vscode2/ToolsMc/master/paper.sh
 wget  https://raw.githubusercontent.com/sebi-vscode2/ToolsMc/master/spigot.sh
-cd /usr/bin
+cd ..
 sudo mkdir toolsmc
-sudo ln -s bukkit.sh /usr/bin/toolsmc
-sudo ln -s paper.sh /usr/bin/paper
-sudo ln -s spigot.sh /usr/bin/spigot
+export PATH=$PATH:/home/toolsmc
