178bbed8e6d90e5c817b8877582440a0f6d9919f
sebi-vscode2 <stoklosasebastian114@gmail.com>
2022-07-22 11:03:12 -0300

    bug fixes

--- prepare.sh
+++ prepare.sh
@@ -13,4 +13,9 @@ cd toolsmc
 wget -O bukkit.sh  https://raw.githubusercontent.com/sebi-vscode2/ToolsMc/master/bukkit.sh
 wget -O paper.sh   https://raw.githubusercontent.com/sebi-vscode2/ToolsMc/master/paper.sh
 wget -O spigot.sh  https://raw.githubusercontent.com/sebi-vscode2/ToolsMc/master/spigot.sh
-export PATH=$PATH:/home/toolsmc
+ln -s /home/toolsmc/bukkit.sh /usr/bin/bukkit
+ln -s /home/toolsmc/paper.sh /usr/bin/paper
+ln -s /home/toolsmc/spigot.sh /usr/bin/spigot
+sudo chmod +777 /usr/bin/bukkit
+sudo chmod +777 /usr/bin/paper
+sudo chmod +777 /usr/bin/spigot
