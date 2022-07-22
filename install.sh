af7661885c838f550088d8e69ef76e3d5e773e4f
sebi-vscode2 <stoklosasebastian114@gmail.com>
2022-07-22 11:51:32 -0300

    added toolsmc to usr/local/bin instead of usr/bin

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo rm /usr/bin/toolsmc
 sudo apt-get update
 sudo apt-get install openjdk-17-jdk -y
 cd /home
@@ -7,6 +8,7 @@ cd toolsmc
 clear
 sudo wget -q  https://raw.githubusercontent.com/sebi-vscode2/ToolsMc/master/toolsmc.sh
 sudo chmod +777 ./toolsmc.sh
+sudo ln -s /home/toolsmc/toolsmc.sh /usr/local/bin/toolsmc
 sudo wget -q  https://raw.githubusercontent.com/sebi-vscode2/ToolsMc/master/prepare.sh 
 sudo chmod +777 prepare.sh
 sudo ./prepare.sh
