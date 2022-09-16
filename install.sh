1fdc6a12b4a359295478c4815eb7be41f891b751
sebi-vscode2 <93952788+sebi-vscode2@users.noreply.github.com>
2022-09-16 12:39:27 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
-sudo rm /usr/bin/toolsmc
 sudo apt-get update
 sudo apt-get install openjdk-17-jdk -y
 cd /home
-sudo mkdir toolsmc
-cd toolsmc
-sudo wget -q https://github.com/sebi-vscode2/ToolsMc/raw/master/version.txt
+sudo mkdir Fastmc
+cd Fastmc
+sudo wget -q https://github.com/sebi-vscode2/FastMc/raw/master/version.txt
 clear
-sudo wget -q  https://raw.githubusercontent.com/sebi-vscode2/ToolsMc/master/toolsmc.sh
-sudo chmod +777 ./toolsmc.sh
-sudo ln -s /home/toolsmc/toolsmc.sh /usr/local/bin/toolsmc
-sudo wget -q  https://raw.githubusercontent.com/sebi-vscode2/ToolsMc/master/prepare.sh 
+sudo wget -q  https://raw.githubusercontent.com/sebi-vscode2/FastMc/master/fastmc.sh
+sudo chmod +777 ./fastmc.sh
+sudo ln -s /home/fastmc/fastmc.sh /usr/local/bin/fastmc
+sudo wget -q  https://raw.githubusercontent.com/sebi-vscode2/FastMc/master/prepare.sh 
 sudo chmod +777 prepare.sh
 sudo ./prepare.sh
 clear
 sudo rm prepare.sh
 clear
-echo "Toolsmc is now installed"
-echo "Now run toolsmc but first reload the terminal" 
+echo "Fastmc is now installed"
+echo "Now run fastsmc but first reload the terminal" 
