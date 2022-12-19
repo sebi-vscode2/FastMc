8770ecba42aeff7449ec095dd496883dbb709882
sebi-vscode2 <stokloasebastian144@proton.me>
2022-12-20 00:00:18 +0100

    Added commandline to installer

--- prepare.sh
+++ prepare.sh
@@ -14,14 +14,17 @@ wget -q -O bukkit.sh  https://raw.githubusercontent.com/sebi-vscode2/FastMc/mast
 wget -q -O paper.sh   https://raw.githubusercontent.com/sebi-vscode2/FastMc/master/paper.sh
 wget -q -O spigot.sh  https://raw.githubusercontent.com/sebi-vscode2/FastMc/master/spigot.sh
 wget -q -O update.sh  https://raw.githubusercontent.com/sebi-vscode2/FastMc/master/update.sh
+wget -q -O commandline.sh https://raw.githubusercontent.com/sebi-vscode2/FastMc/master/commandline.sh
 clear
 sudo chmod +777 /home/Fastmc/paper.sh
 sudo chmod +777 /home/Fastmc/spigot.sh
 sudo chmod +777 /home/Fastmc/bukkit.sh
 sudo chmod +777 /home/Fastmc/update.sh
+sudo chmod +777 /home/Fastmc/commandline.sh
 sudo ln -s /home/Fastmc/bukkit.sh /usr/local/bin/bukkit
 sudo ln -s /home/Fastmc/paper.sh /usr/local/bin/paper
 sudo ln -s /home/Fastmc/spigot.sh /usr/local/bin/spigot
 sudo ln -s /home/Fastmc/update.sh /usr/local/bin/fastupdate
+sudo ln -s /home/Fastmc/commandline.sh /usr/local/bin/fastline
 clear
 
